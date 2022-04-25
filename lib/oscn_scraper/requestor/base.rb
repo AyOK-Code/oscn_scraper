c3cce4c072004c1770eecaa3bfea17c2c002fda8
Holden Mitchell <htmitchell90@gmail.com>
2022-04-25 13:52:35 -0500

    Add headers to requests

--- lib/oscn_scraper/requestor/base.rb
+++ lib/oscn_scraper/requestor/base.rb
@@ -16,7 +16,12 @@ module OscnScraper
 
       def request(url)
         # @queue.shift
-        HTTParty.get(url)
+        HTTParty.get(url, headers: {
+          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+          "Accept-Encoding": "gzip, deflate, br"
+          "Accept-Language": "en-US,en;q=0.9,es;q=0.8",
+          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+          })
       end
 
       def valid_params?(keys, valid_params)
