6e278252350dd932187d23d2fcc1926a899a44e4
Joshua Williams <jwilliams@9bcorp.com>
2022-07-01 16:40:21 -0500

    remade returned object to include array of addresses

--- lib/oscn_scraper/parsers/party_details.rb
+++ lib/oscn_scraper/parsers/party_details.rb
@@ -1,27 +1,9 @@
-require 'byebug'
 module OscnScraper
   module Parsers
     # parses Party Details and returns object with them
     class PartyDetails
       def initialize(html_doc)
         @html_doc = html_doc
-        @details = {
-          personal: {
-            record_date: '1/1/1/1',
-            martial_status: nil,
-            birthday: nil,
-            birth_city: nil,
-            birth_place: nil
-          },
-
-          address: {
-            record_date: '1/1/1/1',
-            status: nil,
-            type: nil,
-            address: nil
-          }
-
-        }
       end
 
       # why?
@@ -29,32 +11,50 @@ module OscnScraper
         new(html_doc).parse
       end
 
-      def parse
-        @details = {
-          personal: {
-            record_date: get_personal(0),
-            martial_status: get_personal(1),
-            birthday: get_personal(2),
-            birth_city: get_personal(3),
-            birth_place: get_personal(4)
-          },
-
-          address: {
-            record_date: get_address(0),
-            status: get_address(1),
-            type: get_address(2),
-            address: get_address(3)
-          }
-
+      def personal_details
+        {
+          record_date: get_personal(0),
+          martial_status: get_personal(1),
+          birthday: get_personal(2),
+          birth_city: get_personal(3),
+          birth_place: get_personal(4),
+          addresses: []
         }
+      end
+
+      def address_details
+        {
+          record_date: nil,
+          status: nil,
+          type: nil,
+          address: nil
+        }
+      end
+
+      def parse
+        party_details = personal_details
+        address_rows.each do |a|
+          party_details[:addresses] << extract_row(a)
+        end
+
+        party_details
       end
 
       def get_personal(num)
         @html_doc.css('.personal tr td')[num].children[0].text.strip
       end
 
-      def get_address(num)
-        @html_doc.css('.addresses tr td')[num].children[0].text.strip
+      def address_rows
+        @html_doc.css('.addresses tbody tr')
+      end
+
+      def extract_row(row)
+        {
+          record_date: row.css('td')[0].text.strip,
+          status: row.css('td')[1].text.strip,
+          type: row.css('td')[2].text.strip,
+          address: row.css('td')[3].text.strip
+        }
       end
     end
   end
