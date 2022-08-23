991d3491459cd886428818d0f7c35859ba0d96b2
Joshua Williams <jwilliams@9bcorp.com>
2022-08-23 16:25:42 -0500

    merged main into branch, and rubocop changes

--- lib/oscn_scraper/parsers/attorney.rb
+++ lib/oscn_scraper/parsers/attorney.rb
@@ -63,8 +63,8 @@ module OscnScraper
       def parse_address(attorney)
         full_address = ''
         attorney.css('td')[0].children[1..].each do |n|
-  n.text == '' ? full_address.concat(' ') : full_address.concat(n.text)
-end
+          n.text == '' ? full_address.concat(' ') : full_address.concat(n.text)
+        end
         full_address.strip
       end
 
