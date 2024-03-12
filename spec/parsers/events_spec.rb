53cca5a0383e7f45e6060ceb9b5fd93e919b1ad3
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2024-03-12 12:40:21 -0500

    Fix failing test after merge

--- spec/parsers/events_spec.rb
+++ spec/parsers/events_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe OscnScraper::Parsers::Events do
       data = described_class.parse(parsed_html)
 
       expect(data[:events].count).to eq 2
-      expect(data[:events].first[:date]).to eq DateTime.new(2023, 10, 19, 14, 1, 0o0, '+0000')
-      expect(data[:events].first).to include({ date: DateTime.new(2023, 10, 19, 14, 1, 0o0, '+0000'),
+      expect(data[:events].first[:date]).to eq DateTime.new(2023, 10, 19, 14, 0, 0o0, '+0000')
+      expect(data[:events].first).to include({ date: DateTime.new(2023, 10, 19, 14, 0, 0o0, '+0000'),
                                                event_type: 'ARRAIGNMENT',
                                                event_code: 'ARR',
                                                party_name: 'EDGAR,  JAMES  KEVIN',
