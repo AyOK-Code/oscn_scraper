f034e861e86a3456fff47eefedd0b1c6616f6d31
sabrinaleggett <sabrina.gelbart@gmail.com>
2024-06-07 15:02:45 -0500

    better date handling

--- spec/parsers/docket_events_spec.rb
+++ spec/parsers/docket_events_spec.rb
@@ -16,6 +16,23 @@ RSpec.describe OscnScraper::Parsers::DocketEvents do
       expect(data[:docket_events].count).to eq 224
     end
 
+    it 'parses docket events with alternative dates' do
+      fixture_path = 'spec/fixtures/parsers/docket_events/alt-date-format.html'
+      parsed_html = load_and_parse_fixture(fixture_path)
+      data = described_class.parse(parsed_html)
+
+      expect(data[:docket_events].count).not_to be nil
+    end
+
+    it 'parses docket events with multiple rows for the same date' do
+      fixture_path = 'spec/fixtures/parsers/docket_events/multirow-for-date.html'
+      parsed_html = load_and_parse_fixture(fixture_path)
+      data = described_class.parse(parsed_html)
+
+      expect(data[:docket_events][1][:date]).to eq data[:docket_events][0][:date]
+      expect(data[:docket_events].last[:date]).not_to eq data[:docket_events][0][:date]
+    end
+
     context 'when a link is in the docket event' do
       it 'returns the links array' do
         fixture_path = 'spec/fixtures/parsers/docket_events/with_link.html'
