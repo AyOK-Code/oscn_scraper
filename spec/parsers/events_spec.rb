b12adbad80ca6d110a9f9f2e4b1d243cf17af6ee
Sabrina Leggett <sabrina.gelbart@gmail.com>
2025-12-09 12:23:51 -0600

    rubo and fix file

--- spec/parsers/events_spec.rb
+++ spec/parsers/events_spec.rb
@@ -1,32 +1,17 @@
 RSpec.describe OscnScraper::Parsers::Events do
   describe '#parse' do
-    it 'parses a the new events format' do
+    it 'parses a case with many events' do
       fixture_path = 'spec/fixtures/parsers/events/new.html'
       html_doc = load_and_parse_fixture(fixture_path)
       parsed_html = html_doc.css('table')
       data = described_class.parse(parsed_html)
 
       expect(data[:events].count).not_to be 0
-      expect(data[:events].first).to include({ date: DateTime.new(2023, 10, 05, 14, 0, 0o0, '+0000'),
+      expect(data[:events].first).to include({ date: DateTime.new(2023, 10, 0o5, 14, 0, 0o0, '+0000'),
                                                event_type: 'PRELIMINARY HEARING CONFERENCE (9-8-23)',
                                                event_code: 'PRELIMC',
                                                party_name: 'MOSHER,  AARON  JOE',
                                                docket: 'Cassandra M Williams' })
-    end
-
-    it 'parses a case with many events' do
-      fixture_path = 'spec/fixtures/parsers/events/multiple.html'
-      html_doc = load_and_parse_fixture(fixture_path)
-      parsed_html = html_doc.css('table')
-      data = described_class.parse(parsed_html)
-
-      expect(data[:events].count).to eq 2
-      expect(data[:events].first[:date]).to eq DateTime.new(2023, 10, 19, 14, 0, 0o0, '+0000')
-      expect(data[:events].first).to include({ date: DateTime.new(2023, 10, 19, 14, 0, 0o0, '+0000'),
-                                               event_type: 'ARRAIGNMENT',
-                                               event_code: 'ARR',
-                                               party_name: 'EDGAR,  JAMES  KEVIN',
-                                               docket: 'Arraignment Docket' })
     end
 
     it 'skips events that do not have a date' do
